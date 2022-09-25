96f9535c7b1fef8d6fd66d884b419d63c9845003
Reza Mirjahanian <rmirjahanian@gmail.com>
2022-09-25 10:26:55 +0330

    tuple

---
+++ src/tuple.py
@@ -0,0 +1,35 @@
+def run(name):
+    # A tuple with mixed datatypes
+    t = (1, 'abc', 1.23, True)
+    print(t)
+    T = (4,)
+    print(type(T))
+    # Not a tuple
+    T = (4)
+    print(type(T))
+    # unpack
+    T = ('red', 'green', 'blue', 'cyan')
+    (a, b, c, d) = T
+    print(a)
+    # Swap
+    a = 1
+    b = 99
+    a, b = b, a
+    print(a)
+    print(b)
+
+    addr = 'bob@python.org'
+    user, domain = addr.split('@')
+    print(user)
+    print(domain)
+    # Replicate
+    T = ('red',) * 3
+    print(T)
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    run('run')
